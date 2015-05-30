70202258a1944098d31daa51010921d452aebb2d
alan <afluder1@mix.wvu.edu>
2015-05-30 18:52:18 -0400

    room maps

--- exercises/webgame.ex52/omicronweb/map.py
+++ exercises/webgame.ex52/omicronweb/map.py
@@ -25,4 +25,35 @@ You dive roll into the armory and scan the room. You see and walk over to the do
 
 the_bridge = Room("The Bridge", 
 """
+The container cracks open and gas starts leaking from it. You burst towards the Bridge with the doomsday device.
+""")
+
+escape_pod("Escape Pod",
+"""
+You inch slowly towards the door, open it and place the doomsday device at your feet. You rush through the ship heading towards the chamber with the escape pods. You see five pods, all with slight damage to them, which do you choose?
+""")
+
+the_end_winner = Room("The End",
+"""
+You jump into pod #2 and hit the eject button. The pod slowly drifts away from the ship deeper and deeper into space. You look back just in time the see the ship explode behind you. 
+""")
+
+the_end_loser = Room("The End",
+"""
+You jump into a random pod and hit eject. However the pod doesn't move. A familar voice then repeats 'I'm afraid I cannot let you do that, Dave' before sealing the pod door. 
+"""
+)
+
+escape_pod.add_paths({
+	'2': the_end_winner,
+	'*': the_end_loser
+})
+
+generic_death = Room("death", "He's dead Jim")
+
+the_bridge.add_paths({
+
+
+
+
 
