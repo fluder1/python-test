a7df8648d5dbf30c6cd4ca01630eb74be9d37880
alan <afluder1@mix.wvu.edu>
2015-06-01 21:17:50 -0400

    html workssss

--- exercises/webgame.ex52/bin/app.py
+++ exercises/webgame.ex52/bin/app.py
@@ -1,19 +1,44 @@
 import web
+from omicronweb import map
 
 urls = (
-    '/hello', 'Index'
+	'/game', 'GameEngine',
+	'/', 'Index',
+    #    '/hello', 'Index'
 )
 
 app = web.application(urls, globals())
+
+#ex52 hack to use debug
+if web.config.get('_session') is None:
+	store = web.session.DiskStore('sessions')
+	session = web.session.Session(app, store, initializer={'room': None})
+	web.config._session = session
+else:
+	session = web.config._session
+
 render = web.template.render('templates/', base="layout")
 
 class Index(object):
+	def GET(self):
+		session.room = map.START
+		web.seeother('/game')
+
+
+class GameEngine(object):
     def GET(self):
-		return render.hello_form()
+		if session.room:
+			return render.show_room(room=session.room)
+		else:
+			return render.you_died()
+
     def POST(self):  
-		form = web.input(name = "Nobody", greet = "Howdy")
-		greeting = "%s, %s" % (form.greet, form.name)
-        	return render.index(greeting = greeting)
+		form = web.input(action=None)
+		
+		if session.room and form.action:
+			session.room = session.room.go(action=form.action)
+
+		web.seeother('/game')		
 
 if __name__ == "__main__":
     app.run()
